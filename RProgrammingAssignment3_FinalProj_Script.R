229e66ede89055087dfcd80f9ccde96baf4526a0
lhsophie <56040523+lhsophie@users.noreply.github.com>
2020-02-05 22:35:23 -0500

    Add files via upload

--- RProgrammingAssignment3_FinalProj_Script.R
+++ RProgrammingAssignment3_FinalProj_Script.R
@@ -3,4 +3,26 @@ X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
 Merged_Data <- cbind(Subject, Y, X)
+<<<<<<< HEAD
+#Step 2:Extract only the measurements on the mean and standard deviation for each measurement.
+TidyData <- Merged_Data %>% select(subject,code,contains("mean"),contains("std"))
+#Step 3:Uses descriptive activity names to name the activities in the data set.
+TidyData$code <- activities[TidyData$code,2]
+#Step 4: Appropriately labels the data set with descriptive variable names.
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+=======
+>>>>>>> bec56fad2981258a64376caf2bdc333b6ef80145
 
